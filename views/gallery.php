f066d10d29552412bc6f8e9fa1538ee145abe38f
lily healey <lily@lilyhealey.co.uk>
2017-07-23 16:19:20 -0400

    add some nav

--- views/gallery.php
+++ views/gallery.php
@@ -1,4 +1,7 @@
 <div id="gallery" class="center hidden">
+  <div id="gallery-ex" onclick="close_gallery();"><img src="/media/svg/ex.svg"></div>
+		<div id="gallery-prev" onclick="prev();"><img src="/media/svg/la.svg"></div>
+		<div id="gallery-next" onclick="next();"><img src="/media/svg/ra.svg"></div>
   <img id="img-gallery" class="center" src="/media/00001.jpeg">
 </div>
 <script type="text/javascript">
