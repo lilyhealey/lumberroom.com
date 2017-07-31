bed8269bb775cbb36486ee243fd258db75fc6b49
lily healey <lily@lilyhealey.co.uk>
2017-07-31 07:07:00 -0400

    simplify index, add exhibitions view

--- index.php
+++ index.php
@@ -4,25 +4,17 @@ $uri = explode("/", trim($_SERVER['REQUEST_URI'], "/"));
 require_once("views/head.php");
 
 // here's where all of the fun stuff is
-if (empty($uri[0]))
-{
+if (empty($uri[0])) {
   require_once("views/main.php");
 }
-else if ($uri[0] == "collection" or $uri[0] == "exhibitions")
-{
-  if (empty($uri[1]))
-    require_once("views/collection.php");
+else {
+  $view = "views/{$uri[0]}.php";
+  if(empty($uri[1]))
+    require_once($view);
   else
     require_once("views/collection-artist.php");
 }
-else if ($uri[0] == "info")
-{
-  require_once("views/info.php");
-}
-else if($uri[0] == "events")
-{
-  require_once("views/events.php");
-}
+
 // close body, close html
 require_once("views/foot.php");
 ?>
