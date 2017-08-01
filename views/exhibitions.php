a873a6b5c9f76402ad2ad9bba159231fdeac7b7d
lily healey <lily@lilyhealey.co.uk>
2017-07-31 20:14:25 -0400

    edit exhibitions view

--- views/exhibitions.php
+++ views/exhibitions.php
@@ -1,1 +1,21 @@
-<p>this is the exhibitions view.</p>
+<?
+$exhibitions = $oo->children($uu->id);
+$base_url = "/{$uu->url}/";
+$ex_urls = array();
+$exhibitions = array_reverse($exhibitions);
+
+?><ul><?
+foreach($exhibitions as $e)
+{
+  $begin = strtotime($e['begin']);
+  $end = strtotime($e['end']);
+
+  $begin_day = date("F j, Y", $begin);
+  $end_day = date("F j, Y", $end);
+
+  $url = $base_url.$e['url'];
+  ?><li><a href="<? echo $url; ?>"><? echo $e['name1']; ?></a><br><? echo $e['deck']; ?>
+  <p><? echo $begin_day; ?> – <? echo $end_day; ?></p></li><hr><?
+}
+?></ul<?
+?>
