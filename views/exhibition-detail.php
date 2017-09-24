c367245e1e99f1a4ef8d5c70ff90502bbef50029
lily healey <lily@lilyhealey.co.uk>
2017-09-24 13:15:11 -0400

    randomise images on exhibition pages

--- views/exhibition-detail.php
+++ views/exhibition-detail.php
@@ -11,14 +11,26 @@ $media_captions = array();
 $i = 0;
 foreach ($media as $m)
 {
+  if (strcasecmp($media['type'], "pdf") != 0)
+  {
   $url = m_url($m);
   $media_urls[] = $url;
   $caption = $m['caption'];
   $media_captions[] = $caption;
-  ?><figure class="thumb" onclick='launch(<? echo $i++; ?>)'><?
+
+  $random_padding = rand(0, 150);
+  $random_width = rand(15, 45);
+  $random_float = (rand(0, 1) == 0) ? 'left' : 'right';
+
+  $style = "padding-top: {$random_padding}px;";
+  $style.= "width: $random_width%;";
+  $style.= "float: $random_float;";
+
+  ?><figure class="thumb" onclick='launch(<? echo $i++; ?>)' style='<? echo $style; ?>'><?
     ?><img src="<? echo $url; ?>"><?
     ?><figcaption><? echo nl2br($caption); ?></figcaption><?
   ?></figure><?
+  }
 }
 require_once("gallery.php");
 ?>
